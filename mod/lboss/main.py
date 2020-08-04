9342783d21908cd23309814e05d7843d79a761ee
MXWXZ <me@imwxz.com>
2020-08-05 06:13:20 +0800

    update lboss

--- mod/lboss/main.py
+++ mod/lboss/main.py
@@ -7,7 +7,7 @@ def Entry():
     while True:
         tm = time.localtime(time.time())
         if tm.tm_hour == 8 and tm.tm_min == 0:
-            utils.SendGroupPlain(666041783, 'LBOSS今天女装了🐴')
+            utils.SendGroupPlain(666041783, '据说LBOSS今天女装！')
             print("[lboss] send")
             time.sleep(86340)   # 24h-60s
         time.sleep(10)
